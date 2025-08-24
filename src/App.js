b124fc93e650c9e70e33e5c6c88de1c48222a73f
Lina <aitmokhtarlina12@gmail.com>
2025-08-24 18:37:30 +0200

    added sigup done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
-import LoginSignup from './Pages/LoginSignup';
+ import LoginSignup from './Pages/LoginSignup';
+
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png'
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './CSS/LoginSignup.css';
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className="loginsignup">
+      <div className="container">
+        <h1>Login</h1>
+        <div className="login-fields">
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Email address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="loginsignup-agree">
+          <input type="checkbox" id="agree" />
+          <label htmlFor="agree">
+            By continuing, I agree to the <b>terms of use & privacy</b>.
+          </label>
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <Link to="/login">Login here</Link>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
